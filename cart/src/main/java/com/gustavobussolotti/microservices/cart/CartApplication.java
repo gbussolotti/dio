9db573048f82d0f8a748faae6f9f4f479043792d
gbussolotti <83429547+gbussolotti@users.noreply.github.com>
2021-06-11 21:08:08 -0300

    Add cart package

--- cart/src/main/java/com/gustavobussolotti/microservices/cart/CartApplication.java
+++ cart/src/main/java/com/gustavobussolotti/microservices/cart/CartApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CartApplication {
 
+	//Teste GIT
 	public static void main(String[] args) {
 		SpringApplication.run(CartApplication.class, args);
 	}
